a4cf4f9a8d1c30238a2773b46e22ed8ab61322b8
DevPatils <dev.patil1006@gmail.com>
2024-06-18 20:12:55 +0530

    Landing page done

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -6,12 +6,14 @@ import hero from '../Components/Images/WhatsApp_Image_2024-06-17_at_16.36.16_04f
 export const Landingpage = () => {
   return (
     <>
-    <Navbar/>
-    <div className="landing_pge">
+   <div className="navbar">
+    <Navbar></Navbar>
+   </div>
+    <div className="landing_page">
         <div className="left">
             <div className="titles-buttons">
-                <h1>United Sports Arena</h1>
-                <h4>Yout ultimate court destination</h4>
+                <h1>U n i t e d &nbsp;  S p o r t s  <br/> Arena <i class="fa-solid fa-bullseye" id='bulls-eye'></i></h1>
+                <h4>"Yout ultimate court destination"</h4>
                 <button>Book Slot</button>
             </div>
         </div>
