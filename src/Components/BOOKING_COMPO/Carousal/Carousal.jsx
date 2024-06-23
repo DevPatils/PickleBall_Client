89737284f7d752345f33f02061433f3abb95d26b
DevPatils <dev.patil1006@gmail.com>
2024-06-23 11:19:34 +0530

    Working on bookings page

--- src/Components/BOOKING_COMPO/Carousal/Carousal.jsx
+++ src/Components/BOOKING_COMPO/Carousal/Carousal.jsx
@@ -33,19 +33,46 @@ export const Carousal = () => {
                     <div className="name-add">
                         <h1>United Sports Pickle Ball Arena</h1>
                         <div className="location">
-                            <i class="fa-solid fa-map-marker-alt"></i>
-                            <h4>Dhairya Prasad Palace, Opp Lal Baug Under Lal Baug Bridge, <br/>Main Gate, beside Patanjali Store, Manjalpur, Vadodara, Gujarat 390011</h4>
+                            <i class="fa-solid fa-map-marker-alt" id='location-marker'></i>
+                            <h4>Dhairya Prasad Palace, Opp Lal Baug Under Lal Baug Bridge, <br />Main Gate, beside Patanjali Store, Manjalpur, Vadodara, Gujarat 390011</h4>
                         </div>
                     </div>
-                    <div className="ratings">
-                        <div className="like-icon">
-                            <i class="fa-solid fa-heart"></i>
+                    <div className="right">
+                        <div className="ratings">
+                            <div className="like-icon">
+                                <i class="fa-solid fa-heart"></i>
+                            </div>
+                            <div className="share-icon" id='share-icon'>
+                                <i class="fa-solid fa-share"></i>
+                            </div>
                         </div>
-                        <div className="share-icon">
-                            <i class="fa-solid fa-share"></i>
+                        <div className="star-rattings">
+                            <i class="fa-solid fa-star"></i> <h4>4.5</h4>
+                        </div>
+                        <div className="directions">
+                            <h1>Best Pickle ball courts in Manjalpur</h1>
+                            <button>Directions</button>
+
                         </div>
                     </div>
 
+                </div>
+            </div>
+            <div className="page-nav">
+                <h1>Book Now</h1>
+                <h1>Details</h1>
+            </div>
+
+            <div className="book-timeline">
+                <div className="first-box">
+                    <i class="fa-solid fa-1"></i>
+                    <div className="details">
+                        <h1>Selected an activity</h1>
+                        <h4>Pickle Ball Courts Outdoors</h4>
+                    </div>
+                    <div className="change-button">
+                        <button>Change</button>
+                    </div>
                 </div>
             </div>
 
